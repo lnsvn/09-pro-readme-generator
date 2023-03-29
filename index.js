2c2468d8752f46c0108b941013c5aa1c7cdeeda8
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2023-03-28 22:31:57 -0400

    Adds functions to render License features

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },                                                              
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining the what, why, and how of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project/get the development environment running?',
+    },
+    {
+        type: 'input',
+        name: 'image',
+        message: 'Paste the file path of an image you would like to include to provide an example of use, starting with "/image"',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide some instructions for the use of your applicatiton.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide guidelines for other developers to contribute to your application or package.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'If you have ran tests on your application, then provide examples on how to run them here.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license have you chosen for your project? If you have chosen a license for a project.',
+        choices: [
+            'MIT', 'Apache', 'BSD', 'Unlicense', 'None'
+        ],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail?',
+    },
+];
 
-// const data = [
-
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?'
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a short description explaining the what, why, and how of your project.',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'What are the steps required to install your project/get the development environment running?',
-        },
-        {
-            type: 'input',
-            name: 'image',
-            message: 'Paste the file path of an image you would like to include to provide an example of use, starting with "/image"',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Provide some instructions for the use of your applicatiton.',
-        },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'Provide guidelines for other developers to contribute to your application or package.',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'If you have ran tests on your application, then provide examples on how to run them here.',
-        },
-        {
-            type: 'input',
-            name: 'license',
-            message: 'What license have you chosen for your project? If you have chosen a license for a project.',
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your github username?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your e-mail?',
-        },
-    ])
-    .then((data) => {
-        fs.writeFile('README.md', generateMarkdown(data), (err) => 
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
         err ? console.log(err) : console.log('Success!! Your README.md file has been generated with your inputs. Check it out!')
-        );
     });
-    
+};
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-
-
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((inputs) => {
+        writeToFile("README.md", generateMarkdown(inputs));
+    });
+};
 
 // Function call to initialize app
 init();
